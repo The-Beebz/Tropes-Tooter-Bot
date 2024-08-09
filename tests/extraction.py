a6f0bd800b74c795c944467e51bd19aabb614292
The Beebz <nyvivianv@gmail.com>
2024-08-09 03:52:31 -0400

    Create extraction.py

---
+++ tests/extraction.py
@@ -0,0 +1,20 @@
+import string
+
+tropeList = './tropes.txt'
+
+# Below is extraction reference
+""" 
+test_string = 'this is a line \n and this is another line \n yet another line is left'
+
+# printing original string
+print (test_string)
+
+# itDivis takes the data of split lines
+itDivis = str.splitlines(test_string)
+
+print("...")
+print("loading")
+print("...")
+# the [1] means its calling the second line in the sheet
+print("The second line was" + itDivis[1]) 
+"""
