ea96170545114b95d04a8c6f833f96039d9ec3e1
The Beebz <nyvivianv@gmail.com>
2024-10-24 17:50:35 -0400

    checking selecting test

--- tests/selector.py
+++ tests/selector.py
@@ -3,11 +3,11 @@ import random
 
 file_path = './Tropes.txt'
 
-def print_random_line():
+def shuffledTropes():
     with open(file_path, 'r') as file:
         
         allTropes = file.readlines()  # Read all lines into a list
-        
-randomTrope = random.choice(allTropes).strip()  # Randomly select a line and strip newline
+        randomTrope = random.choice(allTropes).strip()  # Randomly select a line and strip newline
 
+randomTrope = shuffledTropes()
 print(randomTrope)
