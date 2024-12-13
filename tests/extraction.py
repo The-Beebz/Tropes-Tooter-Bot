861005db308aa32f25adce359980f66d4dbb8bec
The Beebz <nyvivianv@gmail.com>
2024-12-13 01:43:14 -0500

    updated git ignore

--- tests/extraction.py
+++ tests/extraction.py
@@ -2,7 +2,7 @@ import string
 
 tropeList = './tropes.txt'
 
-# Below is extraction reference
+""" # Below is extraction reference
 """ 
 test_string = 'this is a line \n and this is another line \n yet another line is left'
 
@@ -17,4 +17,4 @@ print("loading")
 print("...")
 # the [1] means its calling the second line in the sheet
 print("The second line was" + itDivis[1]) 
-"""
+""" """
